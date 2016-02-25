0c084c4c041605d1acdebb9a42d16a5f8d7da282
Pjotr Prins <pjotr.public01@thebird.nl>
2016-02-25 10:17:46 +0000

    Adding fonts and modules

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(name='piddle',
       version='1.0.15gn',
       include_package_data=True,
       # packages=find_packages()
-      packages=['piddle']
-      )
+      packages=['piddle','piddle/piddleGTK','piddle/piddleSVG','piddle/piddleTK2'],
+      package_data = 
+          {'piddle': ['truetypefonts/*', 'pilfonts/*'] }
+      
+)
+
